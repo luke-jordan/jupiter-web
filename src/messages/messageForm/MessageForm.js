6b5c7257ad9451ec8e79dc6e04b371fe6b5dcb79
taras-d <taras.datsenko@gmail.com>
2019-11-15 14:31:51 +0200

    Update message form

--- src/messages/messageForm/MessageForm.js
+++ src/messages/messageForm/MessageForm.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import classNames from 'classnames';
 
 import { messageDisplayTypeMap } from 'src/core/constants';
 import { mapToOptions } from 'src/core/utils';
 import Input from 'src/components/input/Input';
 import Select from 'src/components/select/Select';
 import TextArea from 'src/components/textArea/TextArea';
+import DatePicker from 'src/components/datePicker/DatePicker';
 import AudienceSelection from 'src/components/audienceSelection/AudienceSelection';
 
 import './MessageForm.scss';
@@ -19,20 +19,6 @@ class MessageForm extends React.Component {
       data: this.messageToFormData(props.message)
     };
   }
-
-  detailsHeaderText = {
-    new: 'Enter message details',
-    view: 'Message details',
-    edit: 'Edit message details',
-    duplicate: 'Edit message details'
-  };
-
-  conditionsHeaderText = {
-    new: 'Specify conditions',
-    view: 'Conditions',
-    edit: 'Edit conditions',
-    duplicate: 'Edit conditions'
-  };
 
   submitButtonText = {
     new: 'Submit',
@@ -55,10 +41,8 @@ class MessageForm extends React.Component {
 
   render() {
     return <form className="message-form" onSubmit={this.submit}>
-      <div className="grid-row">
-        {this.renderDetails()}
-        {this.renderConditions()}
-      </div>
+      {this.renderDetails()}
+      {this.renderConditions()}
       {this.renderAudienceSelection()}
       <div className="form-group text-right">
         <button className="button">{this.submitButtonText[this.props.mode]}</button>
@@ -67,98 +51,126 @@ class MessageForm extends React.Component {
   }
 
   renderDetails() {
-    const { props, state } = this;
-    return <div className="grid-col msg-details">
+    const { state } = this;
+    return <>
       <div className="form-section">
         <div className="section-num">1</div>
-        <div className="section-text">{this.detailsHeaderText[props.mode]}</div>
+        <div className="section-text">Message details</div>
       </div>
-      <div className={classNames('details-box', { 'view-mode': this.isView() })}>
-        {/* Title */}
-        <div className="form-group">
-          <div className="form-label">Title</div>
-          <Input placeholder="Enter title" name="title" disabled={this.isView()}
-            value={state.data.title} onChange={this.inputChange}/>
-        </div>
-        {/* Body */}
-        <div className="form-group">
-          <div className="form-label">Body</div>
-          <TextArea rows="8" placeholder="Enter body" name="body"
-            value={state.data.body} onChange={this.inputChange} disabled={this.isView()}/>
-        </div>
+      {/* Title */}
+      <div className="form-group">
+        <div className="form-label">Title</div>
+        <Input placeholder="Enter title" name="title" disabled={this.isView()}
+          value={state.data.title} onChange={this.inputChange}/>
+      </div>
+      {/* Body */}
+      <div className="form-group">
+        <div className="form-label">Body</div>
+        <TextArea rows="8" placeholder="Enter body" name="body"
+          value={state.data.body} onChange={this.inputChange} disabled={this.isView()}/>
+      </div>
+      <div className="grid-row">
         {/* Quick action */}
-        <div className="form-group">
-          <div className="form-label">Quick action from message</div>
-          <Select placeholder="Select action" name="quickAction" disabled={this.isView()}
-            value={state.data.quickAction} onChange={this.inputChange}>
-            <option value="VIEW_HISTORY">View history</option>
-            <option value="ADD_CASH">Add cash</option>
-            <option value="VISIT_WEB">Visit website</option>
-          </Select>
+        <div className="grid-col-4">
+          <div className="form-group">
+            <div className="form-label">Quick action from message</div>
+            <Select placeholder="Select action" name="quickAction" disabled={this.isView()}
+              value={state.data.quickAction} onChange={this.inputChange}>
+              <option value="VIEW_HISTORY">View history</option>
+              <option value="ADD_CASH">Add cash</option>
+              <option value="VISIT_WEB">Visit website</option>
+            </Select>
+          </div>
         </div>
         {/* Url to visit */}
-        {state.data.quickAction === 'VISIT_WEB' && <div className="form-group">
-          <div className="form-label">Url to visit</div>
-          <Input placeholder="Enter website url" name="urlToVisit" disabled={this.isView()}
-            value={state.data.urlToVisit} onChange={this.inputChange}/>
+        {state.data.quickAction === 'VISIT_WEB' && <div className="grid-col-4">
+          <div className="form-group">
+            <div className="form-label">Url to visit</div>
+            <Input placeholder="Enter website url" name="urlToVisit" disabled={this.isView()}
+              value={state.data.urlToVisit} onChange={this.inputChange}/>
+          </div>
         </div>}
       </div>
-    </div>;
+    </>;
   }
 
   renderConditions() {
-    const { props, state } = this;
-    return <div className="grid-col msg-conditions">
+    const { state } = this;
+    return <>
       <div className="form-section">
         <div className="section-num">2</div>
-        <div className="section-text">{this.conditionsHeaderText[props.mode]}</div>
+        <div className="section-text">Specify conditions</div>
       </div>
-      {/* Type */}
-      <div className="form-group">
-        <div className="form-label">Set display type</div>
-        <Select name="type" disabled={this.isView()}
-          value={state.data.type} onChange={this.inputChange}>
-          {this.typeOptions.map(item => 
-            <option key={item.value} value={item.value}>{item.text}</option>)}
-        </Select>
-      </div>
-      {/* Priority */}
-      <div className="form-group">
-        <div className="form-label">Assign a priority (0=low, 100=high)</div>
-        <Input type="number" name="priority" disabled={this.isView()}
-          value={state.data.priority} onChange={this.inputChange}/>
-      </div>
-      {/* Send message (recurrence) */}
-      <div className="form-group">
-        <div className="form-label">Send message</div>
-        <Select name="recurrence" disabled={this.isView()}
-          value={state.data.recurrence} onChange={this.inputChange}>
-          <option value="ONCE_OFF">Only now</option>
-          <option value="RECURRING">Repeatedly</option>
-          <option value="EVENT_DRIVEN">When some event occurs</option>
-        </Select>
-      </div>
-      {state.data.recurrence === 'RECURRING' && <>
-        {/* Recurring min interval days */}
-        <div className="form-group">
-          <div className="form-label">Assign minimum days between sending messages</div>
-          <Input type="number" name="recurringMinIntervalDays" disabled={this.isView()}
-            value={state.data.recurringMinIntervalDays} onChange={this.inputChange}/>
+      <div className="grid-row">
+        {/* Type */}
+        <div className="grid-col-4">
+          <div className="form-group">
+            <div className="form-label">Set display type</div>
+            <Select name="type" disabled={this.isView()}
+              value={state.data.type} onChange={this.inputChange}>
+              {this.typeOptions.map(item => 
+                <option key={item.value} value={item.value}>{item.text}</option>)}
+            </Select>
+          </div>
         </div>
-        {/* Recurring max in queue */}
-        <div className="form-group">
-          <div className="form-label">Skip sending if more than X messages have been sent</div>
-          <Input type="number" name="recurringMaxInQueue" disabled={this.isView()}
-            value={state.data.recurringMaxInQueue} onChange={this.inputChange} />
+        {/* Send message */}
+        <div className="grid-col-4">
+          <div className="form-group">
+            <div className="form-label">Send message</div>
+            <DatePicker selected={state.data.sendDate} disabled={this.isView()}
+               onChange={value => this.inputChange({ target: { name: 'sendDate', value } })}/>
+          </div>
         </div>
-      </>}
-      {/* Event type and category */}
-      {state.data.recurrence === 'EVENT_DRIVEN' && <div className="form-group">
-        <div className="form-label">Event type and category</div>
-        <Input name="eventTypeCategory" disabled={this.isView()}
-          value={state.data.eventTypeCategory} onChange={this.inputChange}/>
-      </div>}
-    </div>;
+        {/* Priority */}
+        <div className="grid-col-4">
+          <div className="form-group">
+            <div className="form-label">Assign a priority (0=low, 100=high)</div>
+            <Input type="number" name="priority" disabled={this.isView()}
+              value={state.data.priority} onChange={this.inputChange}/>
+          </div>
+        </div>
+      </div>
+      <div className="grid-row">
+        {/* Send message (recurrence) */}
+        <div className="grid-col-4">
+          <div className="form-group">
+            <div className="form-label">Send message</div>
+            <Select name="recurrence" disabled={this.isView()}
+              value={state.data.recurrence} onChange={this.inputChange}>
+              <option value="ONCE_OFF">Only now</option>
+              <option value="RECURRING">Repeatedly</option>
+              <option value="EVENT_DRIVEN">When some event occurs</option>
+            </Select>
+          </div>
+        </div>
+        {state.data.recurrence === 'RECURRING' && <>
+          {/* Recurring min interval days */}
+          <div className="grid-col-4">
+            <div className="form-group">
+              <div className="form-label">Assign minimum days between sending messages</div>
+              <Input type="number" name="recurringMinIntervalDays" disabled={this.isView()}
+                value={state.data.recurringMinIntervalDays} onChange={this.inputChange}/>
+            </div>
+          </div>
+          {/* Recurring max in queue */}
+          <div className="grid-col-4">
+            <div className="form-group">
+              <div className="form-label">Skip sending if more than X messages have been sent</div>
+              <Input type="number" name="recurringMaxInQueue" disabled={this.isView()}
+                value={state.data.recurringMaxInQueue} onChange={this.inputChange} />
+            </div>
+          </div>
+        </>}
+        {state.data.recurrence === 'EVENT_DRIVEN' && <div className="grid-col-4">
+          {/* Event type and category */}
+          <div className="form-group">
+            <div className="form-label">Event type and category</div>
+            <Input name="eventTypeCategory" disabled={this.isView()}
+              value={state.data.eventTypeCategory} onChange={this.inputChange}/>
+          </div>
+        </div>}
+      </div>
+    </>;
   }
 
   renderAudienceSelection() {
@@ -193,6 +205,7 @@ class MessageForm extends React.Component {
         quickAction: 'ADD_CASH',
         type: 'CARD',
         priority: 0,
+        sendDate: new Date(),
         recurrence: 'EVENT_DRIVEN',
         recurringMinIntervalDays: 0,
         recurringMaxInQueue: 0,
@@ -210,6 +223,7 @@ class MessageForm extends React.Component {
       quickAction: defaultTemplate.actionToTake,
       type: defaultTemplate.display.type,
       priority: message.messagePriority,
+      sendDate: null,
       recurrence: message.presentationType,
       recurringMinIntervalDays: recurrenceParameters ? recurrenceParameters.minIntervalDays : 0,
       recurringMaxInQueue: recurrenceParameters ? recurrenceParameters.maxInQueue : 0,
