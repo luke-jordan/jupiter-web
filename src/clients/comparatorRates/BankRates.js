ae57c8ce5de7e90ddaddb1f13af04905ae10cb20
taras-d <taras.datsenko@gmail.com>
2019-11-26 20:37:56 +0200

    Update comparator rates

--- src/clients/comparatorRates/BankRates.js
+++ src/clients/comparatorRates/BankRates.js
@@ -2,58 +2,75 @@ import React from 'react';
 
 import Input from 'src/components/input/Input';
 
-const BankRates = props => {
-  const rateChange = (event, rateIndex) => {
+import trashIcon from 'src/assets/images/trash.svg';
+
+class BankRates extends React.Component {
+  render() {
+    return <div className="bank-rates">
+      {this.renderList()}
+      {this.props.edit && <div className="add-rate">
+        <span className="link text-underline" onClick={this.addClick}>Add rate</span>
+      </div>}
+  </div>;
+  }
+
+  renderList() {
+    const props = this.props;
+
+    if (!props.bank.rates.length) {
+      return null;
+    }
+
+    return <div className="bank-rates-list">
+      {props.bank.rates.map((rate, rateIndex) => {
+        return <div className="bank-rate" key={rateIndex}>
+          <div className="grid-row">
+            <div className="grid-col-6">
+              <span className="rate-label">Above</span>
+              <span className="rate-currency">R</span>
+              {props.edit ? <Input value={rate.above} name="above"
+                onChange={e => this.rateChange(e, rateIndex)}/> :
+                <div className="rate-value">{rate.above}</div>}
+            </div>
+            <div className="grid-col-6">
+              <span className="rate-label">earns</span>
+              {props.edit ? <Input value={rate.earns} name="earns"
+                onChange={e => this.rateChange(e, rateIndex)}/> :
+                <div className="rate-value">{rate.earns}</div>}
+              <span className="rate-currency">%</span>
+            </div>
+          </div>
+          {props.edit && <div className="rate-remove">
+            <img src={trashIcon} alt="remove" onClick={() => this.removeClick(rateIndex)}/>
+          </div>}
+        </div>;
+      })}
+    </div>;
+  }
+
+  rateChange(event, rateIndex) {
     const { name, value } = event.target;
+    const props = this.props;
     const rates = props.bank.rates.slice();
     rates[rateIndex] = { ...rates[rateIndex], [name]: value };
     props.onChange({ ...props.bank, rates });
   };
 
-  const removeClick = rateIndex => {
+  removeClick(rateIndex) {
+    const props = this.props;
     props.onChange({
       ...props.bank,
       rates: props.bank.rates.filter((rate, index) => index !== rateIndex)
     });
   };
 
-  const addClick = () => {
-    props.onChange({
+  addClick = () => {
+    const props = this.props;
+    this.props.onChange({
       ...props.bank,
       rates: [...props.bank.rates, { above: '', earns: '' }]
     });
   };
-
-  return <div className="bank-rates-list">
-    {props.bank.rates.map((rate, rateIndex) => {
-      return <div className="bank-rate" key={rateIndex}>
-        <div className="grid-row">
-          <div className="grid-col-5">
-            <span className="rate-label">Above</span>
-            <span className="rate-currency">R</span>
-            {props.edit ? <Input value={rate.above} name="above"
-              onChange={e => rateChange(e, rateIndex)}/> :
-              <div className="rate-value">{rate.above}</div>}
-          </div>
-          <div className="grid-col-5">
-            <span className="rate-label">earns</span>
-            {props.edit ? <Input value={rate.earns} name="earns"
-              onChange={e => rateChange(e, rateIndex)}/> :
-              <div className="rate-value">{rate.earns}</div>}
-            <span className="rate-currency">%</span>
-          </div>
-          <div className="grid-col-2">
-            {props.edit &&
-              <span className="link text-underline" onClick={() => removeClick(rateIndex)}>Remove rate</span>}
-          </div>
-        </div>
-      </div>;
-    })}
-    {props.edit && 
-      <span className="add-rate">
-        <span className="link text-underline" onClick={addClick}>Add rate</span>
-      </span>}
-  </div>;
 }
 
 export default BankRates;
