b788dc1b93f39248818a533970e7f1fac373d052
luke-grassroot <luke.jordan@grassroot.org.za>
2020-04-06 19:06:22 +0200

    Various fixes, also for triggered boosts now

---
+++ src/components/eventsModal/EventsModal.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Modal from '../modal/Modal';
+
+import './EventsModal.scss';
+
+const events = [
+    ['SAVING_PAYMENT_SUCCESSFUL', 'Save completed'], 
+    ['WITHDRAWAL_EVENT_CONFIRMED', 'Withdrawal finished'],
+    ['USER_CREATED_ACCOUNT', 'Account opened'],
+    ['BOOST_REDEEMED', 'Boost redeemed'],
+    ['SAVING_EVENT_CANCELLED', 'Save cancelled'],
+    ['BOOST_TOURNAMENT_WON', 'Boost tournament won']
+];
+
+const renderTableLines = (systemName, humanName) => (<tr key={systemName}><td>{systemName}</td><td>{humanName}</td></tr>);
+
+const EventsListModal = ({ showEventsModal, onClose }) => 
+    <Modal open={showEventsModal} header="Available message events"
+        className="system-events-list" onClose={onClose}>
+    <table className="table">
+        <thead><tr><th>System name</th><th>Human name</th></tr></thead>
+        <tbody>
+            {events.map((eventPair) => renderTableLines(...eventPair))}
+        </tbody>
+    </table>
+</Modal>;
+
+export default EventsListModal;
